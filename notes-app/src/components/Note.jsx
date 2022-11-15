cef1be4a0ce30992781840a09420bcf775af8121
May Aviram <108925345+MayAviram@users.noreply.github.com>
2022-11-15 21:52:18 +0200

    milestone 4 DONE

--- notes-app/src/components/Note.jsx
+++ notes-app/src/components/Note.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Modal from "react-modal";
+import { useState } from "react";
 
 function Note(props) {
   const {
@@ -14,24 +16,67 @@ function Note(props) {
       onClickDeleteNote(noteIndex);
     }
   };
-  return (
-    <div className="note">
-      <div className="noteContainer dateAndButton">
-        <div className="dateNote" style={{ fontSize: 10, marginBottom: 10 }}>
-          {dateToday}
+
+  const customStyles = {
+    content: {
+      top: "40%",
+      left: "40%",
+      transform: "scale(1)",
+      width: "20%",
+      height: "15%",
+    },
+  };
+
+  Modal.setAppElement("#root");
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    console.log("setIsOpen:false");
+    setIsOpen(false);
+  };
+
+  const noteRender = (classNote) => {
+    return (
+      <div className={`${classNote}note`} onClick={openModal}>
+        <div className="noteContainer dateAndButton">
+          <div className="dateNote" style={{ fontSize: 10, marginBottom: 10 }}>
+            {dateToday}
+          </div>
+          <button
+            className="deleteNoteButton"
+            onClick={(e) => {
+              e.stopPropagation();
+              deleteConfirmation();
+            }}
+          >
+            X
+          </button>
         </div>
-        <button
-          className="deleteNoteButton"
-          onClick={() => {
-            deleteConfirmation();
-          }}
-        >
-          X
-        </button>
+        <div className="noteContainer">
+          <div className="titleNote">{titleOfNote}</div>
+          <div className="textNote">{textOfNote}</div>
+        </div>
       </div>
-      <div className="noteContainer">
-        <div className="titleNote">{titleOfNote}</div>
-        <div className="textNote">{textOfNote}</div>
+    );
+  };
+
+  return (
+    <div>
+      {noteRender("")}
+      <div className="modalNote">
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Note Modal"
+        >
+          {noteRender("modal")}
+          <button onClick={closeModal}>close</button>
+        </Modal>
       </div>
     </div>
   );
